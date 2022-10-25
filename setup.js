3686b52537083a982bb8d005bf6786b9d6cd598e
pakwai <virtue.sign@gmail.com>
2022-10-25 22:57:47 +0800

    Fix manifest file to dynamically generate url for Neo4j endpoints.

--- setup.js
+++ setup.js
@@ -8,8 +8,9 @@ require('dotenv').config();
     const content = fs.readFileSync('manifest-template.yml');
     const template = Handlebars.compile(content.toString());
 
-    const { FORGE_APP_ID } = process.env;
-    const templated = template({ FORGE_APP_ID });
+    const { FORGE_APP_ID, VERCEL_NEO4J_SERVERLESS_URL = '' } = process.env;
+    const NEO4J_REST_ENDPOINT = VERCEL_NEO4J_SERVERLESS_URL.replace('https://', '');
+    const templated = template({ FORGE_APP_ID, NEO4J_REST_ENDPOINT });
 
     fs.writeFileSync('manifest.yml', templated);
   } catch (err) {
